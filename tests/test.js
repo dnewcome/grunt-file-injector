666adb57918bd32b42c5f70230f81d31f4d4fac0
Dan Newcome <dnuke@yahoo-inc.com>
2014-07-14 18:37:35 -0700

    converted manual test to mocha test

--- tests/test.js
+++ tests/test.js
@@ -2,34 +2,40 @@ var grunt = require('grunt');
 var fs = require('fs');
 var chai = require('chai');
 
-// hack to avoid loading a Gruntfile
-// You can skip this and just use a Gruntfile instead
-grunt.task.init = function() {};
 
-// Init config
-grunt.initConfig({
-  'inject-file': {
-    'process-html-template': {
-      'options': {},
-      'files': {
-         'tests/index.html': ['tests/index.html.tpl']
-      }
-    }
-  }
+describe('grunt test', function(done) {
+    it ("shall process template", function(done) {
+
+        // hack to avoid loading a Gruntfile
+        // You can skip this and just use a Gruntfile instead
+        grunt.task.init = function() {};
+
+        // Init config
+        grunt.initConfig({
+          'inject-file': {
+            'process-html-template': {
+              'options': {},
+              'files': {
+                 'tests/index.html': ['tests/index.html.tpl']
+              }
+            }
+          }
+        });
+
+        // load task from file
+        grunt.loadTasks('tasks');
+
+        // Finally run the tasks, with options and a callback when we're done
+        grunt.tasks(['inject-file'], {}, function() {
+          var actual = fs.readFileSync('tests/index.html').toString(); 
+          var expected = 
+        '<html>\n' +
+        '    <h1>Page header</h1>\n' +
+        '\n' +
+        '</html>\n';
+          grunt.log.ok('Done running tasks.');
+          chai.assert.equal(actual, expected);
+          done();
+        });
+    });
 });
-
-// load task from file
-grunt.loadTasks('tasks');
-
-// Finally run the tasks, with options and a callback when we're done
-grunt.tasks(['inject-file'], {}, function() {
-  var actual = fs.readFileSync('tests/index.html').toString(); 
-  var expected = 
-'<html>\n' +
-'    <h1>Page header</h1>\n' +
-'\n' +
-'</html>\n';
-  grunt.log.ok('Done running tasks.');
-  chai.assert.equal(actual, expected);
-});
-
