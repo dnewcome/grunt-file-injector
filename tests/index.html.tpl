1064c4e035c5e9ab66efbb7a50b392f403437066
Dan Newcome <dnuke@yahoo-inc.com>
2014-07-14 18:27:34 -0700

    adding initial grunt test

---
+++ tests/index.html.tpl
@@ -0,0 +1,3 @@
+<html>
+    {{{tests/header.html}}}
+</html>
---
+++ tests/test.js
@@ -0,0 +1,35 @@
+var grunt = require('grunt');
+var fs = require('fs');
+var chai = require('chai');
+
+// hack to avoid loading a Gruntfile
+// You can skip this and just use a Gruntfile instead
+grunt.task.init = function() {};
+
+// Init config
+grunt.initConfig({
+  'inject-file': {
+    'process-html-template': {
+      'options': {},
+      'files': {
+         'tests/index.html': ['tests/index.html.tpl']
+      }
+    }
+  }
+});
+
+// load task from file
+grunt.loadTasks('tasks');
+
+// Finally run the tasks, with options and a callback when we're done
+grunt.tasks(['inject-file'], {}, function() {
+  var actual = fs.readFileSync('tests/index.html').toString(); 
+  var expected = 
+'<html>\n' +
+'    <h1>Page header</h1>\n' +
+'\n' +
+'</html>\n';
+  grunt.log.ok('Done running tasks.');
+  chai.assert.equal(actual, expected);
+});
+
